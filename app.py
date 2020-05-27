7b23efc32dea9ae4f368a925a719a832fd4e2fda
SOCGNA KOUYEM Childéric <westbook90@gmail.com>
2020-05-27 18:07:56 +0100

    V1.0

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html', members=[
+def generate_members():
+    return [
         " SIAKAM Omer", "SOCGNA Childéric",
         "TAOUSSET Abdoul", "TCHEUPI Jonathan",
         "TCHUEM Dimitri", "TCHUEM Nelson", "Wamba Gabin"
-    ])
+    ]
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html', members=generate_members())
 
 
 if __name__ == '__main__':
